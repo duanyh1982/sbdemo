2ab67c70dac9fae712e885ba7b201419cff31e98
duanyh1982 <duanyh1982@gmail.com>
2018-01-07 13:00:11 +0800

    create prj

--- src/main/java/com/example/demo/DemoSb.java
+++ src/main/java/com/example/demo/DemoSb.java
@@ -3,10 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.String;
+
 @SpringBootApplication
 public class DemoSb {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSb.class, args);
 	}
+
+	public String str_prop;
 }
